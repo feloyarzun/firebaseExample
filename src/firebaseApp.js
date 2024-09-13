c92d10895a578b94ddee14bad920e665cc1e9b78
Felipe Oyarzun <fe.oyarzun@gmail.com>
2024-09-13 13:35:16 -0300

    holi

---
+++ src/firebaseApp.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyDWcp_h4LM8ocBFMJj4clLmkOsflbPBOOc',
+  authDomain: 'desafio-3a0da.firebaseapp.com',
+  projectId: 'desafio-3a0da',
+  storageBucket: 'desafio-3a0da.appspot.com',
+  messagingSenderId: '72386621433',
+  appId: '1:72386621433:web:2e04c95dc7233ed28bd728'
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+
+export default app
